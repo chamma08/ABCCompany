229117e0e40bbe1f0408ac2958fd3db4747ee973
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-08-12 19:01:09 +0530

    Created Base module and make the necessary changes

---
+++ base/src/main/java/org/base/dto/OrderEventDTO.java
@@ -0,0 +1,13 @@
+package org.base.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEventDTO {
+    private String message;
+    private String status;
+}
