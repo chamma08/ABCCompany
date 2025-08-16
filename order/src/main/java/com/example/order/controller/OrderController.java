1bbbea8b37a027fee74504dc2b4b20ea710c8fef
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-08-16 13:07:42 +0530

    controller createorder updated

--- order/src/main/java/com/example/order/controller/OrderController.java
+++ order/src/main/java/com/example/order/controller/OrderController.java
@@ -2,7 +2,9 @@ package com.example.order.controller;
 
 import com.example.order.common.OrderResponse;
 import com.example.order.dto.OrderDTO;
+import com.example.order.kafka.OrderProducer;
 import com.example.order.service.OrderService;
+import org.base.dto.OrderEventDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,9 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private OrderProducer orderProducer;
+
     @GetMapping("/getorders")
     public List<OrderDTO>getOrders(){
         return orderService.getAll();
@@ -22,6 +27,10 @@ public class OrderController {
 
     @PostMapping("/createorder")
     public OrderResponse createOrder(@RequestBody OrderDTO orderDTO){
+        OrderEventDTO orderEventDTO = new OrderEventDTO(); //base
+        orderEventDTO.setMessage("Order is commited");
+        orderEventDTO.setStatus("Pending");
+        orderProducer.sendMessage(orderEventDTO);
         return orderService.createOrder(orderDTO);
     }
 
